03edc8ed8ce6134f85e6a11e5d5830e803daf3d3
HarithaMihimal <harithamihimal4@gmail.com>
2024-07-13 20:44:30 +0530

    chore: Update App.js to include routing and components for navigation, feed, video detail, channel detail, and search feed

--- hdtube-platfrom/src/App.js
+++ hdtube-platfrom/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import {Routes,Route,BrowserRouter}from 'react-router-dom'
+import {Box} from '@mui/material'
+import Navbar from './components/Navbar'
+import Feed from './components/Feed'
+import VideoDetail from './components/VideoDetail'
+import ChannelDetail from './components/ChannelDetail'
+import SearchFeed from './components/SearchFeed'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+    <BrowserRouter>
+        <Box sx={{backgroundColor:'#000'}}>
+            <Navbar/>
+            <Routes>
+                <Route path='/'  exact element={<Feed/>}/>
+                <Route path='/video/:id' element={<VideoDetail/>}/>   
+                <Route path='/channel/:id' element={<ChannelDetail/>}/>
+                <Route path='/search/:searchTerm' element={<SearchFeed/>}/>
+
+                {/* https://youtu.be/FHTbsZEJspU?si=5mi8_IOklD6R__Ub */}
+
+            </Routes>
+        </Box>
+    </BrowserRouter>
+)
 
 export default App
---
+++ hdtube-platfrom/src/components/VideoDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const videoDetail = () => {
+  return (
+    <div>videoDetail</div>
+  )
+}
+
+export default videoDetail
