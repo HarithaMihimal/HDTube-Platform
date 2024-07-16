f557b061ca9ee2314974df618b3fe0f2ca1ad04a
HarithaMihimal <harithamihimal4@gmail.com>
2024-07-16 22:42:22 +0530

    Your commit message here

--- hdtube-platfrom/src/App.js
+++ hdtube-platfrom/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Routes,Route,BrowserRouter}from 'react-router-dom'
 import {Box} from '@mui/material'
-import {Navbar,Feed,VideoDetail,ChannelDetail,SearchFeed} from '../components/index'
+import {Navbar,Feed,VideoDetail,ChannelDetail,SearchFeed} from './components'
 
 
 
