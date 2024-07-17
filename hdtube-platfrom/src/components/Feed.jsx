9ed18e06b9d484b707ae08808862e28540ced2ad
HarithaMihimal <harithamihimal4@gmail.com>
2024-07-17 09:31:24 +0530

    refactor: Update Feed and Navbar components

--- hdtube-platfrom/src/components/Feed.jsx
+++ hdtube-platfrom/src/components/Feed.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useEffect,useState } from 'react'
+import {Box,Stack,Typography} from '@mui/material'
+
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{flexDirection:{sx:'column'
+      ,md:'row'
+    }}}>
+    <Box sx={{height:{sx:'auto',md:'92vh'} ,
+  borderRight:'1px solid #3d3d3d', px:{sx:0,md:2}}}>
+    Sidebar
+    <Typography className='copyright'
+    variant='body2' sx={{mt:1.5,color:'#fff'}}>
+      Copyright 2024 Haritha Media
+    </Typography>
+    </Box>
+
+
+    </Stack>
+
   )
 }
 
--- hdtube-platfrom/src/components/index.js
+++ hdtube-platfrom/src/components/index.js
@@ -3,6 +3,6 @@ export { default as Feed } from './Feed.jsx'
 export { default as VideoDetail } from './VideoDetail.jsx'
 export { default as ChannelDetail } from './ChannelDetail.jsx'
 export { default as SearchFeed } from './SearchFeed.jsx'
-
+export { default as SearchBar } from './SearchBar.jsx'
 
 // https://youtu.be/FHTbsZEJspU?si=9Gf6ICvJRPga-vcB
