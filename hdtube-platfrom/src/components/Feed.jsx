2a6337001f069e1993959703b3fec3fb94b3472b
HarithaMihimal <harithamihimal4@gmail.com>
2024-07-17 11:52:56 +0530

    refactor: Update Feed component and add Sidebar component

--- hdtube-platfrom/src/components/Feed.jsx
+++ hdtube-platfrom/src/components/Feed.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { useEffect,useState } from 'react'
 import {Box,Stack,Typography} from '@mui/material'
-
+import Sidebar from './Sidebar'
 
 const Feed = () => {
-  return (
+  
     <Stack sx={{flexDirection:{sx:'column'
       ,md:'row'
     }}}>
     <Box sx={{height:{sx:'auto',md:'92vh'} ,
   borderRight:'1px solid #3d3d3d', px:{sx:0,md:2}}}>
-    Sidebar
+    <Sidebar/>
     <Typography className='copyright'
     variant='body2' sx={{mt:1.5,color:'#fff'}}>
       Copyright 2024 Haritha Media
@@ -20,7 +20,7 @@ const Feed = () => {
 
     </Stack>
 
-  )
+  
 }
 
 export default Feed
--- hdtube-platfrom/src/components/index.js
+++ hdtube-platfrom/src/components/index.js
@@ -4,5 +4,5 @@ export { default as VideoDetail } from './VideoDetail.jsx'
 export { default as ChannelDetail } from './ChannelDetail.jsx'
 export { default as SearchFeed } from './SearchFeed.jsx'
 export { default as SearchBar } from './SearchBar.jsx'
-
+export { default as Sidebar } from './Sidebar.jsx'
 // https://youtu.be/FHTbsZEJspU?si=9Gf6ICvJRPga-vcB
