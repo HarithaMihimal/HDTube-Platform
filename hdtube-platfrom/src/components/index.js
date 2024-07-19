6e086a513300d4db4aeae2f25249fc487f34e581
HarithaMihimal <harithamihimal4@gmail.com>
2024-07-19 21:28:54 +0530

    refactor: Update Videos component to use MUI Stack and Box components

--- hdtube-platfrom/src/components/index.js
+++ hdtube-platfrom/src/components/index.js
@@ -1,9 +1,2 @@
-export { default as Navbar } from './Navbar.jsx'
-export { default as Feed } from './Feed.jsx'
-export { default as VideoDetail } from './VideoDetail.jsx'
-export { default as ChannelDetail } from './ChannelDetail.jsx'
-export { default as SearchFeed } from './SearchFeed.jsx'
-export { default as SearchBar } from './SearchBar.jsx'
-export { default as Sidebar } from './Sidebar.jsx'
-export { default as Videos } from './Videos.jsx'
+
 // https://youtu.be/FHTbsZEJspU?si=9Gf6ICvJRPga-vcB
