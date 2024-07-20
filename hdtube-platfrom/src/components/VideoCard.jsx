e8792554d08b0ec72862cdf7cd0559454ed7be9e
HarithaMihimal <harithamihimal4@gmail.com>
2024-07-20 10:57:39 +0530

    refactor: Improve VideoCard component with link to video and updated styling

--- hdtube-platfrom/src/components/VideoCard.jsx
+++ hdtube-platfrom/src/components/VideoCard.jsx
@@ -9,7 +9,7 @@ const VideoCard = ({video:{id:{videoId},snippet}}) => {
    
 
   return (
-   <Card sx={{width:{md:'320px',xs:'100%'}}}>
+   <Card sx={{width:{md:'320px',xs:'100%'}, boxShadow:'none',borderRadius:0}}>
     <Link to={videoId ? `/video/${videoId}`:demoVideoUrl}>
     <CardMedia image={snippet?.thumbnails?.high?.url}
         alt={snippet?.title}
