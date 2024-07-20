c527990c54f2ead251db974ef27da4a54e96ed4b
HarithaMihimal <harithamihimal4@gmail.com>
2024-07-20 10:54:07 +0530

    refactor: Update VideoCard component to include link to channel and improve styling

--- hdtube-platfrom/src/components/VideoCard.jsx
+++ hdtube-platfrom/src/components/VideoCard.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {Card,Typography,CardMedia,CardContent} from '@mui/material'
 import { demoThumbnailUrl ,demoVideoUrl,demoVideoTitle,demoChannelTitle,demoChannelUrl } from '../utils/constants'
+import { CheckCircle } from '@mui/icons-material'
 
 
 const VideoCard = ({video:{id:{videoId},snippet}}) => {
    
 
   return (
-   <Card>
+   <Card >
     <Link to={videoId ? `/video/${videoId}`:demoVideoUrl}>
     <CardMedia image={snippet?.thumbnails?.high?.url}
         alt={snippet?.title}
@@ -22,6 +23,14 @@ const VideoCard = ({video:{id:{videoId},snippet}}) => {
      <Link to={videoId ? `/video/${videoId}`:demoVideoUrl}>
         <Typography variant='subtitle1' fontWeight='bold' color='#fff' >
             {snippet?.title.slice(0,60) || demoVideoTitle.slice(0,60)}
+        </Typography>
+        </Link>
+
+        <Link to={snippet?.channelId? `/channel/${snippet?.channelId}`:demoChannelUrl}>
+        <Typography variant='subtitle2' fontWeight='bold' color='gray' >
+            {snippet?.channelTitle || demoChannelTitle}
+
+            <CheckCircle sx={{fontSize:12,color:'gray', ml:'5px'}}/>
         </Typography>
         </Link>
     </CardContent> 
