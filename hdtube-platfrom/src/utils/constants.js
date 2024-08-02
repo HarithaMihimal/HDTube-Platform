eba982e4c6f7c00f3060ef19058eb940cc33a9fa
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2024-08-02 20:50:07 +0530

    Update constants.js

--- hdtube-platfrom/src/utils/constants.js
+++ hdtube-platfrom/src/utils/constants.js
@@ -9,8 +9,9 @@ import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural
 import CheckroomIcon from '@mui/icons-material/Checkroom';
 import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+
 
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
