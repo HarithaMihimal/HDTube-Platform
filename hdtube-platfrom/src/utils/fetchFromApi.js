ba5b08a943f4fb200ff4ad1107a602bcc2134df0
HarithaMihimal <harithamihimal4@gmail.com>
2024-08-28 08:42:06 +0530

    refactor: Update X-RapidAPI-Key in fetchFromApi.js

--- hdtube-platfrom/src/utils/fetchFromApi.js
+++ hdtube-platfrom/src/utils/fetchFromApi.js
@@ -7,7 +7,7 @@ const options = {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '6d739cfe38msha06352a293bbbcdp13d267jsn530992b2ce88',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
