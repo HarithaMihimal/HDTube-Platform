a233ef2235e83c5ffd11cb7c75e60b4721f7aed6
HarithaMihimal <harithamihimal4@gmail.com>
2024-08-28 09:20:06 +0530

    refactor: Update border style in Feed component and search results color in SearchFeed component

--- hdtube-platfrom/src/components/Feed.jsx
+++ hdtube-platfrom/src/components/Feed.jsx
@@ -17,7 +17,7 @@ const Feed = () => {
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "2px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
@@ -27,7 +27,7 @@ const Feed = () => {
 
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+          {selectedCategory} <span style={{ color: "#392afc" }}>videos</span>
         </Typography>
 
         <Videos videos={videos} />
--- hdtube-platfrom/src/components/Sidebar.jsx
+++ hdtube-platfrom/src/components/Sidebar.jsx
@@ -17,12 +17,12 @@ const Categories = ({ selectedCategory, setSelectedCategory }) => (
         className="category-btn"
         onClick={() => setSelectedCategory(category.name)}
         style={{
-          background: category.name === selectedCategory && "blue",
+          background: category.name === selectedCategory && "#392afc",
           color: "white",
         }}
         key={category.name}
       >
-        <span style={{ color: category.name === selectedCategory ? "white" : "blue", marginRight: "15px" }}>
+        <span style={{ color: category.name === selectedCategory ? "white" : "#392afc", marginRight: "15px" }}>
           {category.icon}
         </span>
         <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
